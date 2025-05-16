23343118cf905ccd76674d8d851108fac1be54ad
clavijo99 <bclavijogomez@gmail.com>
2025-05-16 09:51:47 -0500

    init repo

---
+++ src/main/java/examples/app/git/AppGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package examples.app.git;
+
+/**
+ *
+ * @author Gomez
+ */
+public class AppGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
